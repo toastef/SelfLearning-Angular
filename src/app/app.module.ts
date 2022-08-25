b4a87a37f2aaeee13e1cdd5d8c249039be0cdb3d
toastef <94532614+toastef@users.noreply.github.com>
2022-08-25 20:42:15 +0200

    First Angular-app, to be continue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser'; // fourni les elements essentiel au projet
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BorderCardDirective } from './border-card.directive';
+import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
+import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
+import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BorderCardDirective,
+    PokemonTypeColorPipe,
+    ListPokemonComponent,
+    DetailPokemonComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/border-card.directive.ts
@@ -0,0 +1,33 @@
+import {Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[pkmnBorderCard]'
+})
+export class BorderCardDirective {
+
+  private initialColor: string = '#f5f5f5';
+  private defaultColor: string = '#009688';
+  private defaultHeight: number = 180;
+
+  constructor(private el: ElementRef) {
+    this.setHeight(this.defaultHeight);
+    this.setBorder(this.initialColor);
+  }
+
+  @Input('pkmBorderCard') borderColor :string;
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.setBorder( this.borderColor || this.defaultColor);
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.setBorder(this.initialColor);
+  }
+  setHeight(height: number){
+    this.el.nativeElement.style.height = `${height}px`;
+  }
+  private setBorder(color: string){
+    let border = 'solid 4px ' + color
+    this.el.nativeElement.style.border = border;
+  }
+}
+
