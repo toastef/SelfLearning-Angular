c8a1cb5e153f12ae32bdba8ad725432f836c48eb
toastef <94532614+toastef@users.noreply.github.com>
2022-09-06 15:15:39 +0200

    authentification Guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {HttpClientInMemoryWebApiModule} from "angular-in-memory-web-api";
 import {InMemoryDataService} from "./in-memory-data.service";
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        PageNotFoundComponent
+        PageNotFoundComponent,
+        LoginComponent
     ],
     imports: [
         BrowserModule,
